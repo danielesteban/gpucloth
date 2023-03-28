951e0e3eba1739276746e8adc709a8bad07f8af0
Daniel Esteban Nombela <dani@gatunes.com>
2023-03-28 02:13:33 +0200

    WebGPU loading/error feedback

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,26 @@ import Simulation from './compute/simulation';
 import { Cloth, Ropes } from './compute/generation';
 
 const Main = (device: GPUDevice) => {
-  const camera = new Camera(device);
-  const renderer = new Renderer(camera, device);
-  const input = new Input(renderer.getCanvas());
-  const simulation = new Simulation(device);
   const dom = document.getElementById('app');
   if (!dom) {
     throw new Error("Couldn't get app DOM node");
   }
+  const camera = new Camera(device);
+  const renderer = new Renderer(camera, device);
+  const input = new Input(renderer.getCanvas());
+  const simulation = new Simulation(device);
+
   dom.appendChild(renderer.getCanvas());
   renderer.setAnimationLoop((command, delta) => (
     simulation.compute(command, delta, input.getPointer(camera), camera.getZoom() * 0.02)
   ));
   renderer.setSize(window.innerWidth, window.innerHeight);
+  simulation.load(Cloth());
 
   const lines = new Lines(camera, device, renderer.getFormat(), renderer.getSamples(), simulation);
   renderer.add(lines);
   const points = new Points(camera, device, renderer.getFormat(), renderer.getSamples(), simulation);
   renderer.add(points);
-
-  simulation.load(Cloth());
 
   input.setHotkeys({
     1: () => simulation.load(Cloth()),
@@ -55,15 +55,15 @@ const Main = (device: GPUDevice) => {
 
 const GPU = async () => {
   if (!navigator.gpu) {
-    throw new Error('WebGPU support');
+    throw new Error("Couldn't load WebGPU");
   }
   const adapter = await navigator.gpu.requestAdapter();
   if (!adapter) {
-    throw new Error("Couldn't get WebGPU adapter");
+    throw new Error("Couldn't load WebGPU adapter");
   }
   const device = await adapter.requestDevice();
   if (!device) {
-    throw new Error("Couldn't get WebGPU device");
+    throw new Error("Couldn't load WebGPU device");
   }
   return device;
 };
@@ -74,4 +74,10 @@ window.addEventListener('dragenter', prevent);
 window.addEventListener('dragover', prevent);
 window.addEventListener('touchstart', prevent);
 
-GPU().then(Main);
+GPU()
+  .then(Main)
+  .catch((e) => {
+    document.getElementById('error')!.innerText = e.message;
+    document.getElementById('support')!.classList.remove('hidden');
+  })
+  .finally(() => document.getElementById('loading')!.classList.add('hidden'));
